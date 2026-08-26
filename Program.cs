9dcc63accf4720ef908019cd37a736bc35672a20
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-26 15:20:47 +0300

    Add search methods with parameterized query

--- Program.cs
+++ Program.cs
@@ -48,9 +48,98 @@ namespace ContactsManager
             }
         }
 
+        static void PrintAllContactsWithFirstName(string firstName)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT* FROM  Contacts  where FirstName = @firstName";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("firstName", firstName);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string FirstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"Contact ID: {contactID}");
+                    Console.WriteLine($"Name: {firstName} {lastName}");
+                    Console.WriteLine($"Email: {email}");
+                    Console.WriteLine($"Phone: {phone}");
+                    Console.WriteLine($"Address: {address}");
+                    Console.WriteLine($"Country ID: {countryID}");
+                    Console.WriteLine();
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Details: {ex.Message}");
+            }
+
+
+
+        }
+
+        static void PrintAllContactsWithFirstNameAndCountry(string firstName,int Country_ID)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT* FROM  Contacts  where FirstName = @firstName";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("firstName", firstName);
+            command.Parameters.AddWithValue("Country_ID", Country_ID);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string FirstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"Contact ID: {contactID}");
+                    Console.WriteLine($"Name: {firstName} {lastName}");
+                    Console.WriteLine($"Email: {email}");
+                    Console.WriteLine($"Phone: {phone}");
+                    Console.WriteLine($"Address: {address}");
+                    Console.WriteLine($"Country ID: {countryID}");
+                    Console.WriteLine();
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Details: {ex.Message}");
+            }
+
+
+
+        }
+
+
         static void Main(string[] args)
         {
-            PrintAllContacts();
+            
+           // PrintAllContacts();
+           // PrintAllContactsWithFirstName("mohammad");
+           // PrintAllContactsWithFirstNameAndCountry("mohammad",1);
         }
     }
 }
