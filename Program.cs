e94c5f60116286b670225b262a20bf8f143db282
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-26 14:42:59 +0300

    Add PrintAllContacts method

--- Program.cs
+++ Program.cs
@@ -8,8 +8,49 @@ namespace ContactsManager
 {
     internal class Program
     {
+        static string connectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=123456;";
+
+        static void PrintAllContacts()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT* FROM  Contacts";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int contactID = (int)reader["ContactID"];
+                    string firstName = (string)reader["FirstName"];
+                    string lastName = (string)reader["LastName"];
+                    string email = (string)reader["Email"];
+                    string phone = (string)reader["Phone"];
+                    string address = (string)reader["Address"];
+                    int countryID = (int)reader["CountryID"];
+
+                    Console.WriteLine($"Contact ID: {contactID}");
+                    Console.WriteLine($"Name: {firstName} {lastName}");
+                    Console.WriteLine($"Email: {email}");
+                    Console.WriteLine($"Phone: {phone}");
+                    Console.WriteLine($"Address: {address}");
+                    Console.WriteLine($"Country ID: {countryID}");
+                    Console.WriteLine();
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Details: {ex.Message}");
+            }
+        }
+
         static void Main(string[] args)
         {
+            PrintAllContacts();
         }
     }
 }
